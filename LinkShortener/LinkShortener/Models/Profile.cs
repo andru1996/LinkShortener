5e963d5381a5bced2330e5da6783ebf3cabc829d
Your Name <you@example.com>
2019-02-19 22:50:56 +0300

    models

---
+++ LinkShortener/LinkShortener/Models/Profile.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LinkShortener.Models
+{
+    public class Profile
+    {
+        public long IdCreator { get; set; }
+        public string name { get; set; }
+        public string Login { get; set; }
+        public string Password { get; set; }
+        public string role { get; set; }
+        public bool StatusBan { get; set; }
+    }
+}
