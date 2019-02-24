8e841f760f28c98d3b432db8076962ed14bc318e
Your Name <you@example.com>
2019-02-24 19:19:28 +0300

    Links pagination

---
+++ LinkShortener/LinkShortener.Gui/Helpers/PagingHelpers.cs
@@ -0,0 +1,34 @@
+﻿using LinkShortener.Gui.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using LinkShortener.GuiCommon.Models;
+
+namespace LinkShortener.Gui.Helpers
+{
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html,
+            PageInfo pageInfo, Func<int, string> pageUrl)
+        {
+            StringBuilder result = new StringBuilder();
+            for (int i = 1; i <= pageInfo.TotalPages; i++)
+            {
+                TagBuilder tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == pageInfo.PageNumber)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            }
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
---
+++ LinkShortener/LinkShortener.GuiCommon/Models/IndexViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkShortener.GuiCommon.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Link> links { get; set; }
+        public PageInfo PageInfo { get; set; }
+    }
+}
